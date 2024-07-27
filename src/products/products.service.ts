8f6b219793d0c50bb69ee900648a5f6679b12cea
gvidal95 <gabrielwv1095@gmail.com>
2024-07-26 19:31:39 -0500

    First commit

---
+++ src/products/products.service.ts
@@ -0,0 +1,89 @@
+import { Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaClient } from '@prisma/client';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
+
+@Injectable()
+export class ProductsService extends PrismaClient implements OnModuleInit {
+
+  private readonly logger = new Logger('Product Service');
+
+  onModuleInit() {
+    this.$connect();
+    this.logger.log('Database connected');
+  }
+
+  create(createProductDto: CreateProductDto) {
+    return this.product.create({
+      data: createProductDto,
+    })
+  }
+
+  async findAll(paginationDto: PaginationDto) {
+
+    const { limit, page } = paginationDto;
+
+    const total = await this.product.count({ where: { available: true } })
+
+    const lastPage = Math.ceil(total / limit);
+
+    return {
+      data: await this.product.findMany({
+        where: { available: true },
+        take: limit,
+        skip: (page - 1) * limit,
+      }),
+      meta: {
+        total,
+        page,
+        lastPage,
+      }
+    }
+  }
+
+  async findOne(id: number) {
+    const product = await this.product.findUnique({
+      where: {
+        id,
+        available: true,
+      }
+    });
+
+    if (!product) throw new NotFoundException(`Product with id ${id} not found`);
+
+    return product;
+  }
+
+  async update(id: number, updateProductDto: UpdateProductDto) {
+
+    const { id: __, ...data } = updateProductDto;
+
+    await this.findOne(id);
+
+    return this.product.update({
+      where: { id },
+      data: data,
+    })
+  }
+
+  async remove(id: number) {
+    await this.findOne(id);
+
+    const product = await this.product.update({
+      where: { id },
+      data: {
+        available: false,
+      }
+    })
+
+    return product;
+
+
+    // return this.product.delete({
+    //   where: {
+    //     id
+    //   }
+    // });
+  }
+}
